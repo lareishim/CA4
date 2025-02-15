121af55cb95340006cc5aebdddf5fa0f47424bee
lareishim <laremgmt@gmail.com>
2025-02-15 17:14:36 +0000

    worked on my ExpensesDAO and also the interface for it

--- src/main/java/DAOs/ExpenseDaoInterface.java
+++ src/main/java/DAOs/ExpenseDaoInterface.java
@@ -1,4 +1,12 @@
+
 package DAOs;
 
+import DTOs.Expense;
+import Exceptions.DAOException;
+
+import java.util.List;
+
 public interface ExpenseDaoInterface {
+
+    List<Expense> getAllExpenses() throws DAOException;
 }
